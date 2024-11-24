9dab5dfec8a145fae7aae4afa85f2d74a31d2138
Vantom2 <108956772+Vantom2@users.noreply.github.com>
2024-11-24 17:38:50 +0100

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,35 +1,42 @@
 #include "Player.h"
 #include <iostream>
 #include <vector>
-
-Player::Player(std::string name) {
+using namespace std;
+Player::Player(string name) {
     this->name = name;
 }
 
 void Player::set_ships_manual() {
-    std::vector<int> ships = {4, 3, 3, 2, 2, 2, 1, 1, 1, 1}; 
+    vector<int> ships = {4, 3, 3, 2, 2, 2, 1, 1, 1, 1}; 
     for (int i = 0; i < ships.size(); i++) {
         bool set = false;
         while (!set) {
             int x, y;
             char ORIENTATION;
-            std::cout << "Ustaw statek o dlugosci " << ships[i] << " (wpisz wspolrzedne x, y oraz orientacje [POZIOMO(H)/PIONOWO(V)]): ";
-            std::cin >> x >> y >> ORIENTATION;
+            cout << "Ustaw statek o dlugosci " << ships[i] << " (wpisz wspolrzedne x, y oraz orientacje [POZIOMO(H)/PIONOWO(V)]): ";
+            cin >> x >> y >> ORIENTATION;
 
             
             if (board.set_ship(x, y, ships[i], ORIENTATION)) {
                 set = true;
-                std::cout << "Statek ustawiony pomyślnie!\n";
+                cout << "Statek ustawiony pomyslnie!\n";
                 board.show_board(false);  
             } else {
-                std::cout << "Nie udało się ustawić statku. Spróbuj ponownie.\n";
+                cout << "Nie udało sie ustawic statku. Sprobuj ponownie.\n";
             }
         }
     }
 }
 
 bool Player::shot_opponent(Board& opponent_board, int x, int y) {
-    return opponent_board.shot(x, y);
+    bool hit = opponent_board.shot(x, y);
+    // Aktualizuj planszę strzałów
+    if (hit) {
+        shot_board.board[y][x] = 'X'; // Trafiony
+    } else {
+        shot_board.board[y][x] = 'P'; // Pudło
+    }
+    return hit;
 }
 
 bool Player::all_ships_destroyed() {
