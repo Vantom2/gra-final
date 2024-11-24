21a5868905b4a671c432fa3d725be6e951df5f71
Vantom2 <108956772+Vantom2@users.noreply.github.com>
2024-11-24 17:38:25 +0100

    Update Board.h

--- Board.h
+++ Board.h
@@ -3,7 +3,8 @@
 
 class Board {
 public:
-    char board[10][10];
+    char board[10][10]; // plansza statkow 
+    char shot_board[10][10]; // Plansza strzalow 
     
     Board();
     void show_board(bool opponent_view = false);
@@ -11,6 +12,10 @@ public:
     bool set_ship(int x, int y, int LENGHT, char ORIENTATION);
     bool shot(int x, int y);
     bool check_all_ships_destroyed();
+    void shown_board(bool is_shot_board);
+    bool receive_shot(int x, int y);
+    void record_shot(int x, int y, bool hit);
+    bool check_hit(int x, int y);
 };
 
 #endif
